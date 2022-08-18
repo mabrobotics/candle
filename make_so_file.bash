34571564124375b2b1f5de891daa729887732e3b
mikab4 <mika.barkan4@gmail.com>
2022-08-18 11:30:24 +0300

    Add script to compile and install

---
+++ make_so_file.bash
@@ -0,0 +1,46 @@
+#!/bin/bash
+usage="$(basename "$0") [-h] [-l launch-file] [-b true|false] [-m true|false] [-s true|false] [-g log_dir] [-o output_folder]
+-- this script runs menteebot light code
+
+where:
+    -p create candlePy.so to create pybind and also install with pip
+    -h print usage
+    "
+
+compile_py=0
+
+while getopts "ph:" flag
+do
+    case "${flag}" in
+        p) compile_py=1;;
+        h) echo "$usage"
+          exit;;
+        *) echo "usage: $0 [-r]" >&2
+           exit;;
+    esac
+done
+
+rm -rf build
+mkdir build
+cd build
+
+if [ $compile_py -eq 1 ]; then
+    echo "#############################Build pybind#################################33"
+    cmake .. -DCANDLE_BUILD_PYTHON=TRUE    
+else
+    echo "#############################Build C++ library#################################33"
+    cmake ..
+fi
+
+make
+
+
+if [ $compile_py -eq 1 ]; then
+    echo "Installing pybind via pip"
+    cp pyCandle/pyCandle* ../candle_pip/src/mab/
+    cd ../candle_pip/
+    pip install .
+fi
+
+echo "I am DONE"
+cd ..
