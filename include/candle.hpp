eeb80b49012df532f67403cdfb727c76b6ee514e
Piotr Wasilewski <piotr.wasilewski@mabrobotics.pl>
2023-03-10 10:33:31 +0100

    feature: updated version to 3.2.2.d

--- include/candle.hpp
+++ include/candle.hpp
@@ -346,7 +346,7 @@ class Candle
    private:
 	const uint8_t VMAJOR = 3;
 	const uint8_t VMINOR = 2;
-	const uint8_t VREVISION = 1;
+	const uint8_t VREVISION = 2;
 	const char VTAG = 'd';
 
 	version_ut candleDeviceVersion;
