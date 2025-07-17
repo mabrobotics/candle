f87f8d10ad608788cc8ca564a03076354e7ba0c3
Jakub Matyszczak <jakub.matyszczak@mabrobotics.pl>
2025-07-17 08:53:18 +0200

    fix: bus update for better bootloader handling

--- src/bus.cpp
+++ src/bus.cpp
@@ -3,51 +3,41 @@
 namespace mab
 {
 
-bool Bus::transfer(char* buffer, int commandLen, int responseLen)
-{
-	/* suppres unused variable warnings */
-	[buffer, commandLen, responseLen] {};
-	return false;
-}
-bool Bus::receive(int responseLen, int timeoutMs, bool checkCrc, bool faultVerbose)
-{
-	[responseLen, timeoutMs, checkCrc, faultVerbose] {};
-	return false;
-}
-void Bus::flushReceiveBuffer()
-{
-	return;
-}
-
-/* public non-virtual functions */
-int Bus::getBytesReceived()
-{
-	return bytesReceived;
-}
-mab::BusType_E Bus::getType()
-{
-	return busType;
-}
-char* Bus::getRxBuffer(int index)
-{
-	return (char*)&rxBuffer[index];
-}
-
-bool Bus::manageMsgErrors(bool ret)
-{
-	msgCnt++;
-	if (ret == false) errorCnt++;
-	if (errorCnt > errorThreshold)
+	bool Bus::transfer(char* buffer, int commandLen, int responseLen)
 	{
-		const char* msg = "Fatal communication error!";
-		std::cout << msg << std::endl;
-		throw msg;
+		/* suppres unused variable warnings */
+		[buffer, commandLen, responseLen] {};
+		return false;
 	}
-	if (msgCnt > msgCntThreshold)
+	bool Bus::receive(int responseLen, int timeoutMs, bool checkCrc, bool faultVerbose)
 	{
-		errorCnt = 0;
-		msgCnt = 0;
+		[responseLen, timeoutMs, checkCrc, faultVerbose] {};
+		return false;
 	}
-	return ret;
-}
-}
+	void Bus::flushReceiveBuffer() { return; }
+
+	/* public non-virtual functions */
+	int			   Bus::getBytesReceived() { return bytesReceived; }
+	mab::BusType_E Bus::getType() { return busType; }
+	char*		   Bus::getRxBuffer(int index) { return (char*)&rxBuffer[index]; }
+
+	bool Bus::manageMsgErrors(bool ret)
+	{
+		msgCnt++;
+		if (ret == false)
+			errorCnt++;
+		else
+			errorCnt = 0;
+		if (errorCnt > errorThreshold)
+		{
+			const char* msg = "Fatal communication error!";
+			throw msg;
+		}
+		if (msgCnt > msgCntThreshold)
+		{
+			errorCnt = 0;
+			msgCnt	 = 0;
+		}
+		return ret;
+	}
+}  // namespace mab
