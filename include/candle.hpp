6e78f36663825afb11482d6c27d164ae7ac1594d
PawelKorsak <p.korsak010@gmail.com>
2025-08-06 12:49:18 +0200

    fix: version bump and failed message removed

--- include/candle.hpp
+++ include/candle.hpp
@@ -340,7 +340,7 @@ class Candle
    private:
 	/* TODO make a proper version class as the reverse initalization is not elegant */
 	const version_ut candleDeviceCompatibleVersion = {'r', 0, 2, 2};
-	const version_ut md80CompatibleVersion = {'r', 0, 4, 2};
+	const version_ut md80CompatibleVersion = {'r', 0, 5, 2};
 
 	static std::vector<Candle*> instances;
 
